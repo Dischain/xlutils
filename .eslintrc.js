1b96f5b51cf7c1d9ff9be18d62490153dc7ca344
Aleksands Shaytanov <rdischain@yandex.ru>
2018-07-16 20:31:07 +0400

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+      "es6": true,
+      "node": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+      "linebreak-style": [
+          "error",
+          "windows",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off"
+  },
+  "plugins": [
+      "react"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "sourceType": "module",
+  }
+};
