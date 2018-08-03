482357372958786f1cc0e6865ba927523c64a53d
Aleksands Shaytanov <rdischain@yandex.ru>
2018-08-03 12:03:19 +0400

    fixed bug

--- test/unit/DocObject.test.js
+++ test/unit/DocObject.test.js
@@ -11,7 +11,7 @@ describe('DocObject', () => {
 
   it('should correctly construct document object', (done) => {
     wsPromise.then((res) => {
-      const docObj = new DocObject('C11', 'C12:I12', res.ws);      
+      const docObj = new DocObject('C11', 'C:I', res.ws);      
       done();
     });
   });
@@ -19,9 +19,9 @@ describe('DocObject', () => {
   // it('should find parent by given parent color', (done) => {
   //   wsPromise.then((res) => {
   //     const parentFG = res.ws.getCell('C11').fill.fgColor,
-  //           docObj1 = new DocObject('C12', 'C12:I12', res.ws, 'C10:C20', parentFG),
-  //           docObj2 = new DocObject('C13', 'C13:I13', res.ws, 'C10:C20', parentFG),
-  //           docObj3 = new DocObject('C13', 'C13:I13', res.ws, 'C10:C20', parentFG);
+  //           docObj1 = new DocObject('C12', 'C:I', res.ws, 'C10:C20', parentFG),
+  //           docObj2 = new DocObject('C13', 'C:I', res.ws, 'C10:C20', parentFG),
+  //           docObj3 = new DocObject('C13', 'C:I', res.ws, 'C10:C20', parentFG);
       
   //     assert.equal(docObj1.getParentCell().getValue(), 'parent2_1');
   //     assert.equal(docObj2.getParentCell().getValue(), 'parent2_1');
@@ -35,10 +35,10 @@ describe('DocObject', () => {
     wsPromise.then((res) => {
       const fg1 = res.ws.getCell('C10').fill.fgColor,
             fg2 = res.ws.getCell('C11').fill.fgColor,
-            docObj1 = new DocObject('C11', 'C11:I11', res.ws, 'C10:C27', null, 'empty', fg2),
-            docObj2 = new DocObject('C15', 'C15:I15', res.ws, 'C10:C27', null, 'empty', fg2),
-            docObj3 = new DocObject('C10', 'C10:I10', res.ws, 'C10:C27', null, fg2, fg1),
-            docObj4 = new DocObject('C19', 'C19:I19', res.ws, 'C10:C27', null, fg2, fg1);
+            docObj1 = new DocObject('C11', 'C:I', res.ws, 'C10:C27', null, 'empty', fg2),
+            docObj2 = new DocObject('C15', 'C:I', res.ws, 'C10:C27', null, 'empty', fg2),
+            docObj3 = new DocObject('C10', 'C:I', res.ws, 'C10:C27', null, fg2, fg1),
+            docObj4 = new DocObject('C19', 'C:I', res.ws, 'C10:C27', null, fg2, fg1);
       
       assert.deepEqual(Object.keys(docObj1.getChildren()), ['obj1', 'obj2', 'obj3']);
       assert.deepEqual(Object.keys(docObj2.getChildren()), ['obj4', 'obj5', 'obj6']);
@@ -57,11 +57,11 @@ describe('DocObject', () => {
       const my1FG = res.ws.getCell('C11').fill.fgColor,
             my2FG = res.ws.getCell('C10').fill.fgColor;
 
-            const docObj1 = new DocObject('C11', 'C11:I11', res.ws, 'C10:C27', my2FG, null, my1FG),
-                  docObj2 = new DocObject('C10', 'C10:I10', res.ws, 'C10:C27', null, null, my2FG),
-                  docObj3 = new DocObject('C15', 'C15:I15', res.ws, 'C10:C27', my2FG, null, my1FG),
-                  docObj4 = new DocObject('C19', 'C19:I19', res.ws, 'C10:C27', null, null, my2FG),
-                  docObj5 = new DocObject('C20', 'C20:I20', res.ws, 'C10:C27', null, null, my1FG);
+            const docObj1 = new DocObject('C11', 'C:I', res.ws, 'C10:C27', my2FG, null, my1FG),
+                  docObj2 = new DocObject('C10', 'C:I', res.ws, 'C10:C27', null, null, my2FG),
+                  docObj3 = new DocObject('C15', 'C:I', res.ws, 'C10:C27', my2FG, null, my1FG),
+                  docObj4 = new DocObject('C19', 'C:I', res.ws, 'C10:C27', null, null, my2FG),
+                  docObj5 = new DocObject('C20', 'C:I', res.ws, 'C10:C27', null, null, my1FG);
       assert.equal(docObj1._getChildrenLimit(), 'C14');
       assert.equal(docObj2._getChildrenLimit(), 'C18');
       assert.equal(docObj3._getChildrenLimit(), 'C18');
