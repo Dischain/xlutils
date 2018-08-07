d782a09265edf61c408819b188ab5979c30374ca
Aleksands Shaytanov <rdischain@yandex.ru>
2018-08-07 12:54:48 +0400

    update;

--- index.js
+++ index.js
@@ -13,40 +13,35 @@ const doc1 = new Doc(doc1Path, sheet, 'E15:E264'),
 //     .then(() => doc1.constructObjects('F:S', null, 'E16', 'E17'))
 //     .then(() => doc2.constructObjects('D:CM', 'C15', 'C16'))
 //     .then(() => doc2.constructObjects('D:CM', null, 'C16', 'C17'))
+// .then(() => {      
+//   const diff = doc1.diff(doc2);
+//   for (let a in diff) {
+//     console.log('Дирекция: ' + a);        
+//     for (let b in diff[a]) {
+//       console.log('Отв. исп.: ' + b);
+//       for (let c in diff[a][b]) {
+//         console.log('Объект: ' + c);
+//       }
+//     }
+//     console.log('----');
+//   }
+// })
+// .catch(console.log);
+
+
 doc1.constructObjects('F:S', null, 'E16', 'empty')
     .then(() => doc2.constructObjects('D:CM', null, 'C16', 'empty'))
     .then(() => {      
       const diff = doc1.diff(doc2);
-      for (let a in diff) {
-        console.log(a);        
+      for (let a in diff) {        
+        console.log('Отв. исп.: ' + a);
+        console.log(diff[a].childrenRange.getAddress().toString());
+        
         for (let b in diff[a]) {
-          console.log('Отв. исп.: ' + b);
-          // for (let c in diff[a][b]) {
-          //   console.log('Объект: ' + c);
-          // }
+          console.log('Объект: ' + b);
+          // for (let keys in diff[a][b]) { console.log(keys); }
         }
         console.log('----');
       }
     })
     .catch(console.log);
-
-// const doc1Path = 'H:/работа/xlutils/test/doc_diff_top1.xlsx',
-//       doc2Path = 'H:/работа/xlutils/test/doc_diff_top2.xlsx';
-
-// const doc1 = new Doc(doc1Path, undefined, 'C10:C27'),
-//       doc2 = new Doc(doc2Path, undefined, 'C10:C21');
-
-// doc1.constructObjects('D:I', 'C10', 'C11')
-//     .then(() => doc2.constructObjects('D:I', 'C10', 'C11'))
-//     .then(() => doc1.constructObjects('D:I', null, 'C11', 'C12'))
-//     .then(() => doc2.constructObjects('D:I', null, 'C11', 'C12'))
-//     .then(() => {
-//        const diff = doc1.diff(doc2);
-//        for (let a in diff) {
-//          console.log(a);
-//          for (let b in diff[a]) {
-//            console.log(b);
-//          }
-//          console.log('----');
-//        }
-//   }).catch(console.log);
