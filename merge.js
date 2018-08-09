f76aa63dc554d4f75434167d9af052ca1d5c4a1b
dischain <rdischain@yandex.ru>
2018-08-09 18:29:27 +0300

    update

--- merge.js
+++ merge.js
@@ -2,27 +2,35 @@
 
 const Doc = require('./lib/objects/Doc');
 
-const doc1Path = 'C:/Users/ShaytanovAI/Downloads/Сводная форма по ГРБС на 02.08.2018_ЭКСПЕРИМЕНТ.XLSX',
-      doc2Path = 'C:/Users/ShaytanovAI/Downloads/Информация_по_заключенным_договорам ТЗ_16.05.18.xlsx',
-      doc3Path = 'C:/Users/ShaytanovAI/Downloads/Сводная форма на 01.05.2018.xlsx',
-      doc4Path = 'C:/Users/ShaytanovAI/Downloads/Информация_по_заключенным_договорам ТЗ_16.05.18 — копия.xlsx',
+const to1Path   = 'C:/Users/ShaytanovAI/Downloads/Информация_по_заключенным_договорам ТЗ_16.05.18.xlsx',
+      from1Path = 'C:/Users/ShaytanovAI/Downloads/Информация_по_заключенным_договорам ТЗ_16.05.18 — копия.xlsx',
+      doc1Path = 'C:/Users/ShaytanovAI/Downloads/Сводная форма по ГРБС на 02.08.2018_ЭКСПЕРИМЕНТ.XLSX',
+      doc3Path = 'C:/Users/ShaytanovAI/Downloads/Сводная форма на 01.05.2018.xlsx',      
       sheet = undefined;
 
 const doc1 = new Doc(doc1Path, sheet, 'E15:E264'),
-      doc2 = new Doc(doc2Path, sheet, 'C15:C230'),
+      to1 = new Doc(to1Path, sheet, 'C15:C230'),
       doc3 = new Doc(doc3Path, sheet, 'C16:C211'),
-      doc4 = new Doc(doc2Path, sheet, 'C15:C230');
+      from1 = new Doc(from1Path, sheet, 'C15:C230');
 
 /**
  * Слияние двух документов с дирекциями и ответственными исполнителями
  */
-doc2.constructObjects('AC:CL', 'C15', 'C16')
-.then(() => doc2.constructObjects('AC:CL', null, 'C16', 'C17'))
-.then(() => doc4.constructObjects('AC:CL', 'C15', 'C16'))
-.then(() => doc4.constructObjects('AC:CL', null, 'C16', 'C17'))
-.then(() => doc2.buildFieldSet('AC8:CL8', 3))
-.then(() => doc4.buildFieldSet('AC8:CL8', 3))
+to1.constructObjects('AC:CQ', 'C15', 'C16')
+.then(() => to1.constructObjects('AC:CQ', null, 'C16', 'C17'))
+.then(() => from1.constructObjects('AC:CQ', 'C15', 'C16'))
+.then(() => from1.constructObjects('AC:CQ', null, 'C16', 'C17'))
+.then(() => to1.buildFieldSet('AC8:CQ8', 3))
+.then(() => from1.buildFieldSet('AC8:CQ8', 3))
 .then(() => {
-  doc4.merge(doc2);
+  from1.merge(to1);
+  console.log(from1._ws.getCell('BM21').value);
+  console.log(from1._ws.getCell('BY24').value);
+  console.log(to1._ws.getCell('BM21').value);
+  console.log(to1._ws.getCell('BY24').value);
+  return to1.save('C:/Users/ShaytanovAI/Documents/saved.xlsx');
+})
+.then(() => {
+  console.log('saved');
 })
 .catch(console.log);
